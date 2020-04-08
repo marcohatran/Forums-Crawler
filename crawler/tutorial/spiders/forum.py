317763d94ddecc2316878ec329e6fba5ef07abda
Tri Nguyen <nvutri@gmail.com>
2020-04-08 16:24:17 +0700

    Add TODO for adding Rules for crawler to follow

--- crawler/tutorial/spiders/forum.py
+++ crawler/tutorial/spiders/forum.py
@@ -5,6 +5,19 @@ class MalwareSpider(scrapy.Spider):
     name = 'forum'
     allowed_domains = ['f319.com']
     start_urls = ['http://f319.com/']
+    
+    # TODO (marco): Add Rule in here to make crawler follow the titles and paginations.
+    # rules = [
+    #   # Link extractor to go to specific property.
+    #    Rule(
+    #        # LinkExtractor for going into each title.
+    #        LinkExtractor(
+    #            restrict_css=('h3.title a'),
+    #        ),
+    #        callback='parse',
+    #        follow=False
+    #    )
+    # ]
 
     def parse(self, response):
         for content in response.css("td[class='info']"):
